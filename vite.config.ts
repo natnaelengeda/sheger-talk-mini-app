2b5e797eef5b97f79a3a63f180afc85192304138
natnael.engeda <natnael.engeda@partner.safaricom.et>
2025-11-04 12:24:37 +0300

    - update vite.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
   css: {
     preprocessorOptions: {
       scss: {
